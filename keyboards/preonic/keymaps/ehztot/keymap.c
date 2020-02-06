4f2b05cca46ad1b6f4c0d6b14d715c88362f30c5
ehz <ptg0317@gmail.com>
2020-02-06 10:29:00 +0900

    rm LT on enter cuz LT only for layer

--- keyboards/preonic/keymaps/ehztot/keymap.c
+++ keyboards/preonic/keymaps/ehztot/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | LOWER| ESC  | GUI  | ALT  |    SPACE    |  del        | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = LAYOUT_preonic_2x2u(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LT(KC_RSFT,KC_ENT), KC_ESC, KC_ESC, KC_LGUI, KC_LALT, KC_SPC, LT(1,KC_DEL),KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
+[_QWERTY] = LAYOUT_preonic_2x2u(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_BSPC, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, KC_ESC, KC_ESC, KC_LGUI, KC_LALT, KC_SPC, LT(1,KC_DEL),KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
