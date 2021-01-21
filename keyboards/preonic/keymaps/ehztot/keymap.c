33cf897584c26fff0a19d87f8a4385e0ddb94194
ehztot <ptg0317@gmail.com>
2021-01-21 10:38:32 +0900

    remapping for TT ______

--- keyboards/preonic/keymaps/ehztot/keymap.c
+++ keyboards/preonic/keymaps/ehztot/keymap.c
@@ -94,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |TT(0) |      |      |             |             | kor  | Hanj | m1   | m2   |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_preonic_2x2u(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_EQL, KC_PSCR, KC_TAB, KC_F11, KC_F12, KC_NO, KC_PAST, KC_PPLS, KC_P7, KC_P8, KC_P9, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS, KC_BRID, KC_BRIU,DM_REC1, KC_PSLS, KC_PMNS, KC_P4, KC_P5, KC_P6, KC_INS, KC_HOME, KC_PGUP, KC_LSFT, KC_VOLD, KC_VOLU,DM_REC2 ,DM_RSTP, KC_P0, KC_P1, KC_P2, KC_P3, KC_DEL, KC_END, KC_PGDN, ALT_TAB, TT(_QWERTY) , KC_NO, KC_TRNS, KC_MPLY, TO(1), KC_HAEN, KC_HANJ,DM_PLY1 , DM_PLY2),
+[_LOWER] = LAYOUT_preonic_2x2u(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_EQL, KC_PSCR, KC_TAB, KC_F11, KC_F12, KC_NO, KC_PAST, KC_PPLS, KC_P7, KC_P8, KC_P9, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS, KC_BRID, KC_BRIU,DM_REC1, KC_PSLS, KC_PMNS, KC_P4, KC_P5, KC_P6, KC_INS, KC_HOME, KC_PGUP, KC_LSFT, KC_VOLD, KC_VOLU,DM_REC2 ,DM_RSTP, KC_P0, KC_P1, KC_P2, KC_P3, KC_DEL, KC_END, KC_PGDN, ALT_TAB,_______, KC_NO, KC_TRNS, KC_MPLY, TO(1), KC_HAEN, KC_HANJ,DM_PLY1 , DM_PLY2),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
